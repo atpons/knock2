a39cd22ab26b1a9620e32e6cbbb0c58ddb69317f
atpons <taiga@atpons.com>
2017-04-13 18:11:15 +0900

    add force quit

--- app.rb
+++ app.rb
@@ -6,37 +6,52 @@ require "pp"
 Dotenv.load
 
 def workseet
-  
+  client = OAuth2::Client.new(
+                  ENV["client_id"],
+                  ENV["client_secret"],
+                  site: "https://accounts.google.com",
+                  token_url: "/o/oauth2/token",
+                  authorize_url: "/o/oauth2/auth")
+  auth_token = OAuth2::AccessToken.from_hash(client,{:refresh_token => ENV["refresh_token"], :expires_at => 3600})
+          auth_token = auth_token.refresh!
+  session = GoogleDrive.login_with_oauth(auth_token.token)
+
+  @ws = session.spreadsheet_by_key(ENV["sheet_id"]).worksheets[0]
 end
-client = OAuth2::Client.new(
-                ENV["client_id"],
-                ENV["client_secret"],
-                site: "https://accounts.google.com",
-                token_url: "/o/oauth2/token",
-                authorize_url: "/o/oauth2/auth")
-auth_token = OAuth2::AccessToken.from_hash(client,{:refresh_token => ENV["refresh_token"], :expires_at => 3600})
-        auth_token = auth_token.refresh!
-session = GoogleDrive.login_with_oauth(auth_token.token)
 
-@ws = session.spreadsheet_by_key(ENV["sheet_id"]).worksheets[0]
+def atend
+  puts "Please input your student id:"
+  @id = gets.chomp!
 
-puts "Please input your student id:"
-@id = gets.chomp!
+  case @id
+  when "00000000"
+    puts "ok, exited"
+    exit
+  end
 
-@ws.rows.each_with_index do |data, i|
-  if data[1].to_s == @id then
-    if data[0] == "0" then
-      puts "Welcome to the room!"
-      @ws[i+1,1] = "1"
-      @ws.save
-    elsif data[0] == "1" then
-      puts "See you again!"
-      @ws[i+1,1] = "0"
-      @ws.save
-    else
-      puts "Sorry, unrecognized flag is opened and you are exited from the room."
-      @ws[i+1,1] = "0" # Force zero
-      @ws.save
+  @ws.rows.each_with_index do |data, i|
+    if data[1].to_s == @id then
+      if data[0] == "0" then
+        puts "Welcome to the room!"
+        @ws[i+1,1] = "1"
+        @ws.save
+      elsif data[0] == "1" then
+        puts "See you again!"
+        @ws[i+1,1] = "0"
+        @ws.save
+      else
+        puts "Sorry, unrecognized flag is opened and you are exited from the room."
+        @ws[i+1,1] = "0" # Force zero
+        @ws.save
+      end
     end
   end
+end
+
+puts "Access to Google Spreadsheet"
+
+workseet
+
+loop do
+  atend
 end
